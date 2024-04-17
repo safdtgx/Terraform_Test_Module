1fe4556008b63187c188a484b8efb4042339819d
safdtgx <inv99@yandex.ru>
2024-04-18 01:03:32 +0300

    comment nat

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
-output "nat-address" {
-  value = yandex_vpc_address.this.external_ipv4_address.0.address
-}
+#output "nat-address" {
+#  value = yandex_vpc_address.this.external_ipv4_address.0.address
+#}
